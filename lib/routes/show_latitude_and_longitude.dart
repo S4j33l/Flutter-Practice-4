30fc028dd2a8ab15c2c5f5542578412072ffc49a
Sajeel <sajeeltipu4903@gmail.com>
2023-09-27 12:45:19 +0500

    yesh

--- lib/routes/show_latitude_and_longitude.dart
+++ lib/routes/show_latitude_and_longitude.dart
@@ -1,20 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:internship_application_4/theme/application_theme.dart';
 
-
-class LatitudeLongitudePage extends StatelessWidget {
+class LatitudeLongitudePage extends StatefulWidget {
   const LatitudeLongitudePage({super.key});
 
   @override
+  State<LatitudeLongitudePage> createState() => _LatitudeLongitudePageState();
+}
+
+class _LatitudeLongitudePageState extends State<LatitudeLongitudePage> {
+  String currentlatitude = "";
+  String currentlongitude = "";
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Text(
+            "Your current latitude is: $currentlatitude",
+            style: appTheme.textTheme.displayMedium,
+          ),
+          Text(
+            "Your current longitude is: $currentlongitude",
+            style: appTheme.textTheme.displayMedium,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text(
+              "Show current latitude and longitude",
+              style: appTheme.textTheme.displayMedium,
+            ),
+          )
+        ],
       ),
     );
   }
